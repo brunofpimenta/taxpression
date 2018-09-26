7a8822b7b938c8b2342c8efdb7ab585058615511
Pimenta <brunofpimenta@gmail.com>
2018-09-26 13:58:13 -0300

    First commit

---
+++ src/main/java/com/brunopimenta/taxpression/TaxpressionApplication.java
@@ -0,0 +1,12 @@
+package com.brunopimenta.taxpression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxpressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxpressionApplication.class, args);
+	}
+}
